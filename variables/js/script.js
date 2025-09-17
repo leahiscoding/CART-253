6591364b87a494dd12c0957338b92bdf119025d2
leahiscoding <leahhjsong@gmail.com>
2025-09-17 16:24:03 -0400

    house project

---
+++ variables/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Title of Project
+ * Leah Song
+ * 
+ * Learning what a variable is and does
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
