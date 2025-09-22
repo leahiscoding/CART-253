9bff2e1f03e29b0ed60f130353e71ccbdeae021b
leahiscoding <leahhjsong@gmail.com>
2025-09-22 13:17:50 -0400

    Finished debugging variables

--- week3/debugging-variables/js/script.js
+++ week3/debugging-variables/js/script.js
@@ -1,6 +1,6 @@
 /**
  * Debugging Variables
- * Pippin Barr
+ * Leah Song
  * 
  * Displays a bug that moves across the screen. But it doesn't work.
  * Because it has bugs.
@@ -8,22 +8,25 @@
 
 "use strict";
 
-const bgColor = "#87ceeb"; // Sky blue
+const bgColour = "#87ceeb"; // Sky blue
 
 // The bug
 const bug = {
     // Position and dimensions
-    x: 250
-    y: 0,
+    x: 250,
+    y: 250, // was 0
     w: 20,
+    h: 30,
+
     // Movement
     velocity: {
-        x: -100,
-        y: -12, // Starts moving down
+        x: 0,
+        y: 12, // Starts moving down
     },
     // Colour
-    color: "#87ceeb" // Dark brown
-
+    // velocity is not moving
+    color: "#654321" // Dark brown
+};
 
 /**
  * Create the canvas
@@ -44,24 +47,28 @@ function draw() {
 }
 
 function moveBug() {
-    bug.xx = bug.x + bug.velocity.x;
-    bug.y == bug.y + bug.velocity.y;
+    bug.x = bug.x + bug.velocity.x;
+    bug.y = bug.y + bug.velocity.y;
 }
 
 /**
  * Displays the bug with its six legs sticking out
  */
 function drawBug() {
+    //console.log("drawBug()");
     // Body
     push();
     noStroke();
     fill(bug.color);
-    ellipse(bug.x, bug.y, bug.width, bug.height);
+    ellipse(bug.x, bug.y, bug.w, bug.h);
     pop();
+
+    //console.log(bug.x, bug.y, bug.w,bug.h)
+    // undefined
 
     // Legs
     push();
-    stroke(bug.stroke);
+    stroke(bug.color);
     // Thicken the legs a bit
     strokeWeight(2);
     // Three lines horizontally across the body at different heights for the legs
