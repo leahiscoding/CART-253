5bcdbef2d9061867dfee6d287c192642e5daf750
leahiscoding <leahhjsong@gmail.com>
2025-09-29 08:26:56 -0400

    Worked on Introducing Event Module

--- Week 5/Introducing events/js/script.js
+++ Week 5/Introducing events/js/script.js
@@ -1,24 +1,37 @@
 /**
- * Title of Project
- * Author Name
+ * Introducing events
+ * Leah Song
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Taking a look at how events work in JavaScript and p5.js
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ * Creates the canvas, makes the canvas black
+ */
 function setup() {
-
+    createCanvas(400,400);
+    background(0);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * 
 */
 function draw() {
 
+}
+
+/**
+ * Draws a circle at the mouse location when the mouse is pressed
+ */
+function mousePressed() {
+    // function will happen when the mouse is pressed
+    // at the exact moment this function is called we know that the mouse is just pressed down
+    push();
+    noStroke();
+    fill(255, 255, 0);
+    ellipse(mouseX, mouseY, 50);
+    pop();
 }
