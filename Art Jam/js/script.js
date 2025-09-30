184f01d694dcb0239dabe345bd380eded8052834
leahiscoding <leahhjsong@gmail.com>
2025-09-30 15:14:53 -0400

    Changed HTML & CSS of art jam project

--- Art Jam/js/script.js
+++ Art Jam/js/script.js
@@ -2,159 +2,110 @@
  * Self-Portrait
  * Leah Song
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * A self-portrait made with raining glowy star (a.k.a stardrops)
  */
 
 "use strict";
 
-/* define properties of a raindrop—deciding what raindrop contains
- blueprint of a raindrop */
-let rainDrop = {
-    x: 0,
-    y: 0,
-    speed: 2,
-    sizeH: 10,
-    sizew: 5,
-    color:{        
-        r: 255,
-        g: 0,
-        b: 0
-    }
-}
 
-// group == array == empty
 
-let rainDropGroup = []
+let starDropGroup = []
 
 
 function setup(){
-
-    createCanvas(600, 600);
-
-    for(let rainDropCounter=0; rainDropCounter<300; rainDropCounter=rainDropCounter+1){
-            rainDropGroup.push({
+// create canvas for stardrops
+    createCanvas(1000, 600);
+// using for loop to create multiple stardrops
+    for(let starDropCounter=0; starDropCounter<400; starDropCounter += 1){
+        // stardropcounter gets bigger by 1 each time until it hits 300
+            starDropGroup.push({
         x: random (0, width),
         y: random (0, height),
-        speed: random (0.5,1),
-        sizeH: 10,
-        sizew: 5,
+        // speed of the stardrop
+        speed: random (0.5,0.8),
+        // size referece for stars
+        sizeH: 5,
+        sizew: 11,
+        // colour of the star drop—stardrops are lighter colour
         color:{        
-            r: random (0,255),
-            g: random (0,255),
-            b: random (0,255)
+            r: random (90,255),
+            g: random (90,255),
+            b: random (90,255)
         }
-        // this is now pushing 100 times
+    
 });
     }
-//     rainDropGroup.push({
-//         x: 0,
-//         y: 0,
-//         speed: 2,
-//         sizeH: 10,
-//         sizew: 5,
-//         color:{        
-//             r: 255,
-//             g: 0,
-//             b: 0
-//         }
-// });
-//     rainDropGroup.push({
-//     x: 20,
-//     y: 0,
-//     speed: 2,
-//     sizeH: 10,
-//     sizew: 5,
-//     color:{        
-//         r: 0,
-//         g: 0,
-//         b: 255
-//     }
-// });
-
 
 }
-// set up only works one time, but good place to set up the list of raindrops
 
 /**
  * Canvas setup
- * it doesn't work until you call it
 */
 function draw() {
     background(0);
-    // draw
-    drawRainDrop();
-    // move
-    moveRainDrop();
+    /*adding glowy effect to each stars because shiny things are pretty 
+    source: https://github.com/Creativeguru97/YouTube_tutorial/blob/master/p5_hacks/Glow_effect/glowing_shapes/sketch.js*/
+    drawingContext.shadowBlur = 10;
+    drawingContext.shadowColor = color(255, 255, 255);
+     // draw the stardrops
+    drawStarDrop();
+    // move the stardrops
+    moveStarDrop();
 }
 
 // action: make raindrop
-function drawRainDrop() {
-    // push();
-    // noStroke();
-    // // raindrop falls from the sky
-    // fill(rainDropGroup[0].color.r, rainDropGroup[0].color.g, rainDropGroup[0].color.b);
-    // rect(rainDropGroup[0].x, rainDropGroup[0].y, rainDropGroup[0].sizew, rainDropGroup[0].sizeH);
-    // // raindrop.x because we already defined it in the blueprint
-    // pop();
-
-    // push();
-    // noStroke();
-    // // raindrop falls from the sky
-    // fill(rainDropGroup[1].color.r, rainDropGroup[1].color.g, rainDropGroup[1].color.b);
-    // rect(rainDropGroup[1].x, rainDropGroup[1].y, rainDropGroup[1].sizew, rainDropGroup[1].sizeH);
-    // // raindrop.x because we already defined it in the blueprint
-    // pop();
-// instead of repeating the same code, we use a loop 
+function drawStarDrop() {
 
 //for loop - for each rainDrop in the group draw each one
-for(let position = 0 ; position<300 ; position=position+1)
+    for(let position = 0 ; position<200 ; position=position+1)
     // no randomizing for loop
-    /* position is the index of the array - position is in the group of the raindrops - how they're placed within the group
-        trying to access specific element, raindrop, of the group*/
     {
     push();
     noStroke();
-    fill(rainDropGroup[position].color.r, rainDropGroup[position].color.g, rainDropGroup[position].color.b);
-    rect(rainDropGroup[position].x, rainDropGroup[[position]].y, rainDropGroup[position].sizew, rainDropGroup[position].sizeH);
+    // using position to get each stardrop's colour
+    fill(starDropGroup[position].color.r, starDropGroup[position].color.g, starDropGroup[position].color.b);
     // instead of putting number 0 or 1, we put position
+    star (starDropGroup[position].x, starDropGroup[position].y, 4, 9, 5);
+    // size and npoint of the stars
     pop();
     }
 }
 
-function moveRainDrop() {
-//     rainDropGroup[0].y = rainDropGroup[0].y + rainDropGroup[0].speed;
-//     if (rainDropGroup[0].y > height) {
-//         // condition is true
-//         rainDropGroup[0].y = 0;
-//     }
-//     // a loop–condition to reset the raindrop
-
-//     rainDropGroup[1].y = rainDropGroup[1].y + rainDropGroup[1].speed;
-//     if (rainDropGroup[1].y > height) {
-//         // condition is true
-//         rainDropGroup[1].y = 0;
-//     }
-//     // a loop–condition to reset the raindrop
-
-// }
-
-    for(let position = 0 ; position<300 ; position=position+1)
+function moveStarDrop() {
+//for loop - for each rainDrop in the group move each one
+    for(let position = 0 ; position<200 ; position=position+1)
     {
     
-     rainDropGroup[position].y = rainDropGroup[position].y + rainDropGroup[position].speed;
-    if (rainDropGroup[position].y > height) {
-        // condition is true
-        rainDropGroup[position].y = 0;
-        rainDropGroup[position].speed += random (0.5,0.6);
+     starDropGroup[position].y = starDropGroup[position].y + starDropGroup[position].speed;
+    if (starDropGroup[position].y > height) {
+        // resetting the raindrop to the top
+        starDropGroup[position].y = 0;
+        starDropGroup[position].speed += random (0.2,0.3);
         // you need to add [position] to get particular raindrop
     }
     // checking if the height is greater than the canvas height
     // manipulating properties of each raindrop
 
-
-    // instead of putting number 0 or 1, we put position
+     // instead of putting number 0 or 1, we put position 
+   
   
     }
 }
 
+// source:https://stackoverflow.com/questions/62893486/what-is-the-math-behind-creation-of-a-star-in-p5js?noredirect=1&lq=1
+function star(x, y, radius1, radius2, npoints) {
+    let angle = TWO_PI / npoints;
+    let halfAngle = angle / 2.0;
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += angle) {
+      let sx = x + cos(a) * radius2;
+      let sy = y + sin(a) * radius2;
+      vertex(sx, sy);
+      sx = x + cos(a + halfAngle) * radius1;
+      sy = y + sin(a + halfAngle) * radius1;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
+  }
+
+// Thank you Sabine for helping me write this wonderful code!
