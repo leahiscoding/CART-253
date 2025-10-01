3fa2dc6e4f7226925e078accea5381b949ea2f5f
leahiscoding <leahhjsong@gmail.com>
2025-10-01 16:29:03 -0400

    Finished Plain Javascript Module

--- Week 5/Plain Javascript Event/js/script.js
+++ Week 5/Plain Javascript Event/js/script.js
@@ -1,24 +1,53 @@
 /**
- * Title of Project
- * Author Name
+ * Plain Javascript Event
+ * Leah Song
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Experimenting with event handling in Plain Javascript
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+// Information about the current and possible background fills
+const bg = {
+    fill: "#000000",
+    fills: {
+        black: "#000000",
+        white: "#FFFFFF"
+    },
+    switchKey: 32 // Space Bar
+};
+
+/**d
+ * Creates the canvas
 */
 function setup() {
+    createCanvas (400, 400);
+    
+    // Listen for keypresses
+    window.addEventListener("keydown", changeBG);
+    // addEventListener > I would like to know when this thing happens
+    // keydown > key is pressed down
+    // changeBG > when that happens, run this function
+}
 
+/**
+ * Displays the background
+*/
+function draw() {
+    background(bg.fill);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
-
+ * Switches the background from black to white
+ */
+function changeBG (event) {
+    if (event.keyCode === bg.switchKey) {
+        if (bg.fill === bg.fills.black) {
+            bg.fill = bg.fills.white;
+        }
+        else {
+            bg.fill = bg.fills.black;
+        }
+    }
 }
