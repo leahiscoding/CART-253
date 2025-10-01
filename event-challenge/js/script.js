09151ca8b1f75183b7151ca4dd2004ddabe38e1b
leahiscoding <leahhjsong@gmail.com>
2025-10-01 19:43:58 -0400

    Make the loop for keyboard

--- event-challenge/js/script.js
+++ event-challenge/js/script.js
@@ -1,24 +1,73 @@
 /**
- * Title of Project
- * Author Name
+ * Event-Challenge
+ * Weini, Ziyan, Leah
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Doing an event challenge
  */
 
 "use strict";
 
+// Current score
+let score = 0;
+
+// Is the game over?
+let gameOver = false;
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ * Create the canvas
+ */
 function setup() {
-
+  createCanvas(400, 400);
 }
-
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Update the score and display the UI
+ */
 function draw() {
-
+  background("#87ceeb");
+  
+  // Only increase the score if the game is not over
+  if (!gameOver) {
+    // Score increases relatively slowly
+    score += 0.05;
+  }
+  displayUI();
 }
+
+/**
+ * Show the game over message if needed, and the current score
+ */
+function displayUI() {
+  if (gameOver) {
+    push();
+    textSize(48);
+    textStyle(BOLD);
+    textAlign(CENTER, CENTER);
+    text("You lose!", width/2, height/3);
+    pop();
+  }
+  displayScore();
+}
+
+/**
+ * Display the score
+ */
+function displayScore() {
+  push();
+  textSize(48);
+  textStyle(BOLD);
+  textAlign(CENTER, CENTER);
+  text(floor(score), width/2, height/2);
+  pop();
+}
+
+
+function lose()
+{
+    gameOver = true
+}
+
+function keyReleased(){
+    lose ();
+}
+
