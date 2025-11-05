51833a9580fdae5afa069a34c365c4516e7e42cb
leahiscoding <leahhjsong@gmail.com>
2025-11-05 12:35:24 -0500

    added pepe picture to the file and added keyispressed function

--- mod-jam/js/modjam.js
+++ mod-jam/js/modjam.js
@@ -13,19 +13,32 @@
  * https://p5js.org/
  */
 "use strict";
+
+// Game state variable
+let gameState = "start";
+let score = 0;
+
 //Set up canvas
 function setup() {
-    createCanvas(800, 500);
+    createCanvas(800, 600);
     startScreen();
 }
 
 //Start screen
 function startScreen (){
     background (0);
-    fill (255);
     textAlign (CENTER);
+    textFont('Courier New');
     textSize (32);
+    fill("#66FF33");
     text ("Space Frog", width/2, height/2 - 40);
     textSize (16);
     text ("Press SPACE to start game", width/2, height/2+80);
+}
+
+// Press space to start game
+function keyPressed() {
+    if (key === ' ') {
+        gameState = "play";
+    }
 }
