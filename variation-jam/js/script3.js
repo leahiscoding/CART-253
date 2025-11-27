0be48dc892843ba2ff6a0cda5f2ee8eb993cdc6d
Leah Song <leahhjsong@gmail.com>
2025-11-26 20:39:38 -0500

    added noise function to the background

--- variation-jam/js/script3.js
+++ variation-jam/js/script3.js
@@ -1,10 +1,28 @@
-"use strict";
-
-// global variables
 
 
-function setup() {
-    
-    createCanvas (windowWidth, windowHeight);
-    background (0);
+function setup () {
+    createCanvas(windowWidth, windowHeight)
 }
+
+function draw () {
+    
+    noStroke();
+     console.log (frameCount);
+    if (frameCount % 5 === 0){
+    background (0);
+    for (let i = 2; i < width; i +=random (0,25)){
+        for (let j = 2; j < height; j +=random (0,25)){
+            const n = noise(i *0.05, j *0.05, frameCount * 0.01);
+            fill(n*random (100,255), n * random(100,255), n * random(100,255));
+            ellipse(i, j, 2,2);
+        
+    }
+    }
+}    
+}
+
+function reportWindowSize() {
+  }
+  
+  window.onresize = reportWindowSize;
+  
